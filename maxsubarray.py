564b1f5016c40b69609b9a823cd884e7ae751a23
Aishwarya <aishwarya.singh.cs28@iilm.edu>
2026-04-07 13:24:12 +0530

    Create maxsubarray.py

---
+++ maxsubarray.py
@@ -0,0 +1,24 @@
+'''Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.'''
+def maxSubArray(self, nums: List[int]) -> int:
+        res=nums[0]
+        t=0
+        for i in nums:
+            if t<0:
+                t=0
+            t+=i
+            res=max(res,t)
+        return res
+        
